1c5f1d100f57ce8a58740a3e74b50946ea53f67a
Danillo Alves <113880934+DanilloAlvesSouza@users.noreply.github.com>
2025-07-19 22:16:45 -0300

    usando variavel de ambiente para plotar a curva

--- app/handler.py
+++ app/handler.py
@@ -1,28 +1,31 @@
 import json
 from services.curva_service import processar_futuros_di
 import matplotlib.pyplot as plt
+import os  # Adicione esta linha
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def handler(event, context):
     try:
         for record in event['Records']:
-            body = json.loads(record['body'])  # body ainda é uma string JSON
+            body = json.loads(record['body'])
 
             if body.get('Categoria') == 'Futuros_DI':
                 insumos = body.get('insumos', [])
                 curvaFinalizada = processar_futuros_di(insumos)
 
-                # --- Visualização com matplotlib ---
-                
-                prazos = [p['Vencimento'] for p in curvaFinalizada]
-                valores = [p['PU'] for p in curvaFinalizada]
-                plt.plot(prazos, valores, marker='o')
-                plt.title('Curva de Juros Interpolada')
-                plt.xlabel('Prazo (dias úteis)')
-                plt.ylabel('PU ou Taxa')
-                plt.grid(True)
-                plt.show()
-                # -----------------------------------
+                # --- Visualização com matplotlib apenas em ambiente local ---
+                if os.environ.get("AMBIENTE") == "local":
+                    prazos = [p['Vencimento'] for p in curvaFinalizada]
+                    valores = [p['PU'] for p in curvaFinalizada]
+                    plt.plot(prazos, valores, marker='o')
+                    plt.title('Curva de Juros Interpolada')
+                    plt.xlabel('Prazo (dias úteis)')
+                    plt.ylabel('PU ou Taxa')
+                    plt.grid(True)
+                    plt.show()
+                # ----------------------------------------------------------
 
             else:
                 print("Categoria desconhecida ou não suportada.")
