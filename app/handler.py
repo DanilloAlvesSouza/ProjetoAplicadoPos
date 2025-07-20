3d41ad0ee20a96339a7d372e2b308abc005bba94
Danillo Alves <113880934+DanilloAlvesSouza@users.noreply.github.com>
2025-07-19 22:08:20 -0300

    feat: implementando visualizacao com matplotlib

--- app/handler.py
+++ app/handler.py
@@ -1,5 +1,7 @@
 import json
 from services.curva_service import processar_futuros_di
+import matplotlib.pyplot as plt
+
 
 def handler(event, context):
     try:
@@ -8,8 +10,20 @@ def handler(event, context):
 
             if body.get('Categoria') == 'Futuros_DI':
                 insumos = body.get('insumos', [])
-                resultadoPrazoCalculado = processar_futuros_di(insumos)
-                print("Resultado do processamento:", resultadoPrazoCalculado)
+                curvaFinalizada = processar_futuros_di(insumos)
+
+                # --- Visualização com matplotlib ---
+                
+                prazos = [p['Vencimento'] for p in curvaFinalizada]
+                valores = [p['PU'] for p in curvaFinalizada]
+                plt.plot(prazos, valores, marker='o')
+                plt.title('Curva de Juros Interpolada')
+                plt.xlabel('Prazo (dias úteis)')
+                plt.ylabel('PU ou Taxa')
+                plt.grid(True)
+                plt.show()
+                # -----------------------------------
+
             else:
                 print("Categoria desconhecida ou não suportada.")
 
